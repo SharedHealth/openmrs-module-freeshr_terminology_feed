60f64e71e4a0239fbee25be42778d240eb8ba820
Balaji Narain <narainbalaji@gmail.com>
2014-05-23 10:22:51 +0530

    Utsab, BalajiN | #77 | Initial commit

---
+++ api/src/main/java/org/openmrs/module/freeshr/terminology/feed/FeedActivator.java
@@ -0,0 +1,37 @@
+package org.openmrs.module.freeshr.terminology.feed;
+
+import org.openmrs.module.ModuleActivator;
+
+public class FeedActivator implements ModuleActivator {
+
+    @Override
+    public void willRefreshContext() {
+
+    }
+
+    @Override
+    public void contextRefreshed() {
+
+    }
+
+    @Override
+    public void willStart() {
+
+    }
+
+    @Override
+    public void started() {
+
+    }
+
+    @Override
+    public void willStop() {
+
+    }
+
+    @Override
+    public void stopped() {
+
+    }
+
+}
