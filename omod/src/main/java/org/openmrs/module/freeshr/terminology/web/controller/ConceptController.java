227ffee5210f918b1517a416b0353c7b7a79296c
Balaji Narain <balajin@thoughtworks.com>
2014-07-29 15:39:30 +0530

    Soham, Shurthi, BalajiN | Allowing the client to pass basic auth for concepts end point

--- omod/src/main/java/org/openmrs/module/freeshr/terminology/web/controller/ConceptController.java
+++ omod/src/main/java/org/openmrs/module/freeshr/terminology/web/controller/ConceptController.java
@@ -1,21 +1,56 @@
 package org.openmrs.module.freeshr.terminology.web.controller;
 
+import org.openmrs.api.APIAuthenticationException;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.freeshr.terminology.web.api.Concept;
 import org.openmrs.module.freeshr.terminology.web.api.mapper.ConceptMapper;
+import org.openmrs.module.webservices.rest.SimpleObject;
+import org.openmrs.module.webservices.rest.web.RestConstants;
+import org.openmrs.module.webservices.rest.web.RestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping(value = "/rest/v1/tr/concepts")
 public class ConceptController {
 
+    private static final String DISABLE_WWW_AUTH_HEADER_NAME = "Disable-WWW-Authenticate";
+
     @Autowired
     private ConceptMapper mapper;
+
+    /**
+     * @should return unauthorized if not logged in
+     * @should return forbidden if logged in
+     */
+    @ExceptionHandler(APIAuthenticationException.class)
+    @ResponseBody
+    public SimpleObject apiAuthenticationExceptionHandler(Exception ex, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        int errorCode;
+        String errorDetail;
+        if (Context.isAuthenticated()) {
+            // user is logged in but doesn't have the relevant privilege -> 403 FORBIDDEN
+            errorCode = HttpServletResponse.SC_FORBIDDEN;
+            errorDetail = "User is logged in but doesn't have the relevant privilege";
+        } else {
+            // user is not logged in -> 401 UNAUTHORIZED
+            errorCode = HttpServletResponse.SC_UNAUTHORIZED;
+            errorDetail = "User is not logged in";
+            if (shouldAddWWWAuthHeader(request)) {
+                response.addHeader("WWW-Authenticate", "Basic realm=\"OpenMRS at " + RestConstants.URI_PREFIX + "\"");
+            }
+        }
+        response.setStatus(errorCode);
+        return RestUtil.wrapErrorResponse(ex, errorDetail);
+    }
+
+    private boolean shouldAddWWWAuthHeader(HttpServletRequest request) {
+        return request.getHeader(DISABLE_WWW_AUTH_HEADER_NAME) == null || !request.getHeader(DISABLE_WWW_AUTH_HEADER_NAME).equals("true");
+    }
 
     @RequestMapping(value = "/{uuid}", method = RequestMethod.GET)
     @ResponseBody
