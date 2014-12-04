612c3252488b1b2a119c3fc10e97d791ec7c39ca
Utsab Banerjee <utsabban@thoughtworks.com>
2014-12-04 20:38:21 +0530

    Utsab | BDSHR - 393 | Added stored procedures for event creation

---
+++ omod/src/main/resources/storedProcedures/AddConceptEventsProc.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE add_concept_events (new_concept_id INT,
+                              class_name VARCHAR(255))
+
+BEGIN
+
+  DECLARE db_uuid INT;
+  DECLARE uri VARCHAR(255);
+
+  select uuid into db_uuid from concept where concept_id = @new_concept_id;
+  select concat('/openmrs/ws/rest/v1/tr/concepts/', @db_uuid) into uri;
+
+  insert into event_records(uuid, title, category, uri, object) values(uuid(), 'concept', 'concept', @uri, @uri);
+  insert into event_records(uuid, title, category, uri, object) values(uuid(), @class_name, @class_name, @uri, @uri);
+
+END;
---
+++ omod/src/main/resources/storedProcedures/AddConceptReferenceEventProc.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE add_concept_reference_events (new_concept_reference_id INT)
+
+BEGIN
+
+  DECLARE db_uuid INT;
+  DECLARE uri VARCHAR(255);
+
+  select uuid into db_uuid from concept_reference_term where concept_reference_term_id = @new_concept_reference_term_id;
+  select concat('/openmrs/ws/rest/v1/tr/referenceterms/', @db_uuid) into uri;
+  insert into event_records(uuid, title, category, uri, object) values(uuid(), 'ConceptReferenceTerm', 'ConceptReferenceTerm', @uri, @uri);
+
+END;
