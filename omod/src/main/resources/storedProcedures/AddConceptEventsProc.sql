ee039577a2fd2b0092e5524b215dd911fd6bb936
Nehashri <nehashrl@thoughtworks.com>
2015-04-21 14:45:12 +0530

    Neha | bdshr-603 | cleaning up migrations.

--- omod/src/main/resources/storedProcedures/AddConceptEventsProc.sql
+++ omod/src/main/resources/storedProcedures/AddConceptEventsProc.sql
@@ -1,10 +1,11 @@
 CREATE PROCEDURE add_concept_events (new_concept_id INT,
-                              class_name VARCHAR(255))
+                              class_name VARCHAR(255),
+                              url VARCHAR(255))
 
 BEGIN
 
   select uuid into @db_uuid from concept where concept_id = new_concept_id;
-  select concat('/openmrs/ws/rest/v1/tr/concepts/', @db_uuid) into @uri;
+  select concat(url, @db_uuid) into @uri;
 
   insert into event_records(uuid, title, category, uri, object) values(uuid(), 'concept', 'concept', @uri, @uri);
   insert into event_records(uuid, title, category, uri, object) values(uuid(), class_name, class_name, @uri, @uri);
--- omod/src/main/resources/storedProcedures/AddConceptReferenceEventProc.sql
+++ omod/src/main/resources/storedProcedures/AddConceptReferenceEventProc.sql
@@ -1,9 +1,10 @@
-CREATE PROCEDURE add_concept_reference_events (new_concept_reference_term_id INT)
+CREATE PROCEDURE add_concept_reference_events (new_concept_reference_term_id INT,
+                                              url VARCHAR(255))
 
 BEGIN
 
   select uuid into @db_uuid from concept_reference_term where concept_reference_term_id = new_concept_reference_term_id;
-  select concat('/openmrs/ws/rest/v1/tr/referenceterms/', @db_uuid) into @uri;
+  select concat(url, @db_uuid) into @uri;
 
   insert into event_records(uuid, title, category, uri, object) values(uuid(), 'ConceptReferenceTerm', 'ConceptReferenceTerm', @uri, @uri);
 
