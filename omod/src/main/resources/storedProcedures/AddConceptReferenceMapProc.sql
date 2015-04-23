5428d6806e5f5e871716cd86e919a49ee32cbf50
Nehashri <nehashrl@thoughtworks.com>
2015-04-23 18:24:46 +0530

    Neha | bdshr-000 | fixing concept reference map procedure.

--- omod/src/main/resources/storedProcedures/AddConceptReferenceMapProc.sql
+++ omod/src/main/resources/storedProcedures/AddConceptReferenceMapProc.sql
@@ -1,4 +1,5 @@
-CREATE PROCEDURE add_concept_reference_map (concept_reference_term_id INT, concept_id INT,
+CREATE PROCEDURE add_concept_reference_map (INOUT concept_reference_term_id_var INT,
+        concept_id INT,
         concept_source_identifier INT,
         reference_term VARCHAR(255),
         reference_name VARCHAR(255),
@@ -8,8 +9,9 @@ CREATE PROCEDURE add_concept_reference_map (concept_reference_term_id INT, conce
         INSERT INTO concept_reference_term (concept_source_id, name, code, creator, date_created, uuid)
         VALUES (concept_source_identifier, reference_name, reference_term, 1, now(), uuid());
 
-        select LAST_INSERT_ID() into concept_reference_term_id;
+        select concept_reference_term_id into concept_reference_term_id_var from concept_reference_term
+        where name = reference_name and code = reference_term and concept_source_id = concept_source_identifier;
 
         INSERT INTO concept_reference_map (concept_reference_term_id, concept_map_type_id, creator, date_created, concept_id, uuid)
-        VALUES(concept_reference_term_id, reference_type_id, 1, now(), concept_id, uuid());
+        VALUES(concept_reference_term_id_var, reference_type_id, 1, now(), concept_id, uuid());
 END;
