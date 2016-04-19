9828e67ca98847bace8b9f07e6b14e16e0c0ba94
nehashri <nehashrl@thoughtworks.com>
2016-04-19 13:18:59 +0530

    Neha | bdshr-1209 | forming only the base url in UrlUtil.

--- omod/src/main/java/org/openmrs/module/freeshr/terminology/utils/UrlUtil.java
+++ omod/src/main/java/org/openmrs/module/freeshr/terminology/utils/UrlUtil.java
@@ -16,17 +16,17 @@ public class UrlUtil {
     public String getRequestURL(HttpServletRequest request) {
         String requestUrl = getServiceUriFromRequest(request);
         if (requestUrl == null) {
-            requestUrl = getBaseUrlFromOpenMrsGlobalProperties(request);
+            requestUrl = getBaseUrlFromOpenMrsGlobalProperties();
         }
-        return requestUrl != null ? requestUrl : formUrl(request.getScheme(), request.getServerName(), request.getServerPort(), request.getRequestURI(), request.getQueryString());
+        return requestUrl != null ? requestUrl : formUrl(request.getScheme(), request.getServerName(), request.getServerPort());
     }
 
-    private String getBaseUrlFromOpenMrsGlobalProperties(HttpServletRequest request) {
+    private String getBaseUrlFromOpenMrsGlobalProperties() {
         String restUri = Context.getAdministrationService().getGlobalProperty("webservices.rest.uriPrefix");
         if (StringUtils.isNotBlank(restUri)) {
             try {
                 URI uri = new URI(restUri);
-                return formUrl(uri.getScheme(), uri.getHost(), uri.getPort(), request.getRequestURI(), request.getQueryString());
+                return formUrl(uri.getScheme(), uri.getHost(), uri.getPort());
             } catch (URISyntaxException e) {
                 logger.warn("Invalid url is set in global property webservices.rest.uriPrefix");
             }
@@ -39,18 +39,15 @@ public class UrlUtil {
         if (scheme == null) {
             return null;
         }
-        return formUrl(scheme, request.getServerName(), request.getServerPort(), request.getRequestURI(), request.getQueryString());
+        return formUrl(scheme, request.getServerName(), request.getServerPort());
     }
 
-    private String formUrl(String scheme, String hostname, int port, String path, String queryString) {
+    private String formUrl(String scheme, String hostname, int port) {
         String url = null;
         if (port != 80 && port != 443 && port != -1) {
-            url = scheme + "://" + hostname + ":" + port + path;
+            url = scheme + "://" + hostname + ":" + port;
         } else {
-            url = scheme + "://" + hostname + path;
-        }
-        if (queryString != null) {
-            return url + "?" + queryString;
+            url = scheme + "://" + hostname;
         }
         return url;
     }
